4dfc52edc21ebea2b1a2eda6c4fd896f8e1f8c96
chenjie <chenjie@virtaitech.com>
2025-05-14 22:08:18 +0200

    add install package

--- scripts/installPackages.R
+++ scripts/installPackages.R
@@ -1,1 +1,3 @@
+# install packages
+install.packages("ica", repos="https://ftp.fau.de/cran")
 install.packages("multichull", repos="https://ftp.fau.de/cran")
--- scripts/testSimulationData.R
+++ scripts/testSimulationData.R
@@ -11,5 +11,6 @@ source("./functions/ILS_CJICA.R")
 
 
 set.seed(123)
-data_Nk50_K5_Q44566_E04_VAF08 <- Simulate_CJICA(Nk = 50, Vm = 2500, K = 5, Qvect = c(4,4,5,6,6), E = .4, M = 2, type = 1, cor = .05, VAF = 0.8)
-save(data_Nk50_K5_Q44566_E04_VAF08, file = "./data/data_Nk50_K5_Q44566_E04_VAF08.RData")
+data_Nk50_Vm500_K2_Q33_E04_VAF1 <- Simulate_CJICA(Nk = 50, Vm = 500, K = 2, Qvect = c(3,3), E = .4, M = 2, type = 1, cor = .05, VAF = 1)
+save(data_Nk50_Vm500_K2_Q33_E04_VAF1, file = "./data/data_Nk50_Vm500_K2_Q33_E04_VAF1.RData")
+
